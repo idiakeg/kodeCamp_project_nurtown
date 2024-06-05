7eb4f2aad8d513614313b2dec50baec1573e228b
idiakeg <idiakeg@gmail.com>
2024-06-05 22:43:09 +0100

    responsive navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { links } from "../../utils";
 import { NavLink } from "react-router-dom";
 import nav_logo from "../../assets/nav_logo.svg";
 import "./Navbar.css";
 
 const Navbar = () => {
+    const [hamburgerToggle, setHamburgerToggle] = useState(false);
     return (
         <nav>
             <div className="wrapper nav_container">
                 <div className="nav_logo">
                     <img src={nav_logo} alt="digital project logo" />
                 </div>
-                <ul className="nav_items">
+                <ul
+                    className={`nav_items ${
+                        hamburgerToggle ? "show_nav" : "hide_nav"
+                    }`}
+                >
                     {links.map((item, index) => (
                         <li key={index}>
                             <NavLink
@@ -25,6 +30,14 @@ const Navbar = () => {
                         </li>
                     ))}
                 </ul>
+                <div
+                    onClick={() => setHamburgerToggle((prev) => !prev)}
+                    className={`hamburger ${hamburgerToggle ? "close" : ""}`}
+                >
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
         </nav>
     );
