f5daeb0744fc4aacd1235b9c1d428ce89740de39
idiakeg <idiakeg@gmail.com>
2024-06-06 02:59:50 +0100

    Project page.

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { projects } from "../../utils";
+import forward from "../../assets/forward_arrow.svg";
+import "./Projects.css";
+import Pagination from "../../components/pagination/Pagination";
 
 const Projects = () => {
-    return <div>Projects</div>;
+    return (
+        <section>
+            <div className="wrapper project_container">
+                <p className="project_head">
+                    <span>Our</span> <br /> Projects
+                </p>
+                <div className="gallery_separator"></div>
+                <div className="project_item_container">
+                    {projects.map((item, index) => (
+                        <div key={index} className="project_item">
+                            <div className="project_image">
+                                <img src={item.image} alt={item.title} />
+                            </div>
+                            <div className="project_info">
+                                <p className="project_title">{item.title}</p>
+                                <p className="project_body">{item.body}</p>
+                                <span className="project_viewmore">
+                                    View More{" "}
+                                    <img src={forward} alt="view more" />
+                                </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <Pagination />
+            </div>
+        </section>
+    );
 };
 
 export default Projects;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,7 @@
+import proj1 from "./assets/projects/project1.svg";
+import proj2 from "./assets/projects/project2.svg";
+import proj3 from "./assets/projects/project3.svg";
+
 const links = [
     { path: "/", name: "main" },
     { path: "/gallery", name: "gallery" },
@@ -6,4 +10,22 @@ const links = [
     { path: "/contacts", name: "contacts" },
 ];
 
-export { links };
+const projects = [
+    {
+        image: proj1,
+        title: "Sample Project",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+        image: proj2,
+        title: "Sample Project 2",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+    {
+        image: proj3,
+        title: "Sample Project 3",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+    },
+];
+
+export { links, projects };
