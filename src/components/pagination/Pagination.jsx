a51de82e117e0207eb72a86a22c336568f9c368e
idiakeg <idiakeg@gmail.com>
2024-06-06 02:58:37 +0100

    dummy pagination

---
+++ src/components/pagination/Pagination.jsx
@@ -0,0 +1,21 @@
+import "./Pagination.css";
+import forward from "../../assets/forward_arrow.svg";
+import backward from "../../assets/back_arrow.svg";
+
+const Pagination = () => {
+    return (
+        <div className="pagination">
+            <span>01</span>
+            <span className="pagination_slash"></span>
+            <span>05</span>
+            <span className="pagination_direction">
+                <img src={forward} alt="go back" />
+            </span>
+            <span className="pagination_direction">
+                <img src={backward} alt="go forward" />
+            </span>
+        </div>
+    );
+};
+
+export default Pagination;
